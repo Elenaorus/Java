9dcd53e24ab9513a365dd28caa1c00c6059ece2e
Elenaorus <110274316+Elenaorus@users.noreply.github.com>
2022-12-02 23:03:36 +0700

    add Home work 3

--- src/HomeWork3/Task3.java
+++ src/HomeWork3/Task3.java
@@ -1,9 +1,10 @@
 package HomeWork3;
 
-import java.util.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Task23 {
+public class Task3 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         List<Integer> listResult = new ArrayList<>();
@@ -13,14 +14,11 @@ public class Task23 {
         list.add(150);
         list.add(1);
         list.add(195);
-        for (int i : list) {
-            if (i % 2 == 0){
-                listResult.add(i);
-            }
-        }
-        list.removeAll(listResult);
-        System.out.println(list);
-        System.out.println(listResult);
 
+
+        System.out.println(list);
+
+        System.out.println(Collections.max(list));
+        System.out.println(Collections.min(list));
     }
 }
